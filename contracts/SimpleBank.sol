85154a61ae57411595db90999769ce35d3706703
MortalMelvin <67949165+MortalMelvin@users.noreply.github.com>
2020-11-16 04:07:35 +1000

    Update SimpleBank.sol

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -1,10 +1,10 @@
 /*
-    This exercise has been updated to use Solidity version 0.5.16
+    This exercise has been updated to use Solidity version 0.6.12
     Breaking changes from 0.5 to 0.6 can be found here: 
     https://solidity.readthedocs.io/en/v0.6.12/060-breaking-changes.html
 */
 
-pragma solidity ^0.5.16;
+pragma solidity ^0.6.12;
 
 contract SimpleBank {
 
